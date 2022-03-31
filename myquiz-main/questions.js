45527a93832a51df8c739502a849ff2891b40aff
Mocariza <mcocariza@gmail.com>
2022-03-31 16:17:04 +0800

    commit all

---
+++ myquiz-main/questions.js
@@ -0,0 +1,62 @@
+var questions =[
+    {
+num : 1,
+question : "Which HTML element defines the title of a document?",
+answer : "meta",
+options : [
+"title",
+"meta",
+"head",
+"body"
+        ]
+    },
+    {
+num: 2,
+question: "The <h1> element in the HTML defines",
+answer: "Headings",
+options: [
+"Headings",
+"Hyperlink",
+"HyperText",
+"Html-text"
+]
+},
+{
+num: 3,
+question: "A HTML element known to be <ins>, defines",
+answer: "Insertion",
+options: [
+"Incapsulation",
+"Insertion",
+"Instance",
+"Instantiation"
+]
+},
+
+{
+num: 4,
+question: "The <mark> element in HTML displays content as",
+answer: "Highlighted",
+options: [
+"Bold",
+"Important",
+"Emphasized",
+"Highlighted"
+]
+},
+{
+num: 5,
+question: "The HTML element known to be <del>, defines",
+answer: "Removed data",
+options: [
+"Deleted data",
+"Removed data",
+"Crossed data",
+"Overwritten data"
+]
+},
+
+
+
+
+];
