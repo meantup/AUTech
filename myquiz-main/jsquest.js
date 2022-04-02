12d928f8c55e7238bd456aceb7c9919b2925cd43
Mocariza <mcocariza@gmail.com>
2022-04-02 15:48:36 +0800

    initial commit for changes

--- myquiz-main/jsquest.js
+++ myquiz-main/jsquest.js
@@ -1,53 +1,106 @@
-var jsquestions =[
-    {
-    question : "Inside which HTML element do we put the JavaScript?",
-    answer : "<script>",
-    options : [
-    "&lt;javascript&gt;",
-    "&lt;java&gt;",
-    "&lt;js&gt;",
-    "&lt;script&gt;"
-    ]
-    },
-    {
-    question: "Where is the correct place to insert a JavaScript?",
-    answer: "Both the <head> section and the <body> section",
-    options: [
-    "Both the &lt;head&gt; section and the &lt;body&gt; section",
-    "The &lt;head&gt; section",
-    "The &lt;body&gt; section",
-    "The &lt;render&gt; section"
-    ]
-    },
-    {
-    question: 'How do you write '+"Hello World"+' in an alert box?',
-    answer: 'alert("Hello World")',
-    options: [
-    'msg("Hello World")',
-    'alertBox("Hello World")',
-    'msgBox("Hello World")',
-    'alert("Hello World")'
-    ]
-    },
+// var jsquestions =[
+//     {
+//     question : "Inside which HTML element do we put the JavaScript?",
+//     answer : "<script>",
+//     options : [
+//     "&lt;javascript&gt;",
+//     "&lt;java&gt;",
+//     "&lt;js&gt;",
+//     "&lt;script&gt;"
+//     ]
+//     },
+//     {
+//     question: "Where is the correct place to insert a JavaScript?",
+//     answer: "Both the <head> section and the <body> section",
+//     options: [
+//     "Both the &lt;head&gt; section and the &lt;body&gt; section",
+//     "The &lt;head&gt; section",
+//     "The &lt;body&gt; section",
+//     "The &lt;render&gt; section"
+//     ]
+//     },
+//     {
+//     question: 'How do you write '+"Hello World"+' in an alert box?',
+//     answer: 'alert("Hello World")',
+//     options: [
+//     'msg("Hello World")',
+//     'alertBox("Hello World")',
+//     'msgBox("Hello World")',
+//     'alert("Hello World")'
+//     ]
+//     },
     
-    {
-    question: "How do you create a function in JavaScript?",
-    answer: "function myFunction()",
-    options: [
-    "function:myFunction()",
-    "function myFunction()",
-    "function = myFunction()",
-    "myFunction()"
-    ]
-    },
-    {
-    question: "How to write an IF statement in JavaScript?",
-    answer: "if(i==5)",
-    options: [
-    "if i==5 then",
-    "if i=5 then",
-    "if i=5",
-    "if(i==5)"
-    ]
-    },
-    ];
+//     {
+//     question: "How do you create a function in JavaScript?",
+//     answer: "function myFunction()",
+//     options: [
+//     "function:myFunction()",
+//     "function myFunction()",
+//     "function = myFunction()",
+//     "myFunction()"
+//     ]
+//     },
+//     {
+//     question: "How to write an IF statement in JavaScript?",
+//     answer: "if(i==5)",
+//     options: [
+//     "if i==5 then",
+//     "if i=5 then",
+//     "if i=5",
+//     "if(i==5)"
+//     ]
+//     },
+//     ];
+var jsquestions =[
+{
+question : "Inside which HTML element do we put the JavaScript?",
+answer : "script",
+options : [
+"javascript",
+"js",
+"scripting",
+"script"
+]
+},
+{
+question: "What is the correct syntax for referring to an external script called (xxx.js)?",
+answer: "script src=(xxx.js)",
+options: [
+"script href=(xxx.js)",
+"script src=(xxx.js)",
+"script name=(xxx.js)",
+"None of the above"
+]
+},
+{
+question: "How do you create a function in JavaScript?",
+answer: "function myFunction()",
+options: [
+"function myFunction()",
+"function:myFunction()",
+"function = myFunction()",
+"None of the above"
+]
+},
+
+{
+question: "How to write an IF statement in JavaScript?",
+answer: "if (i == 5)",
+options: [
+"if i = 5",
+"if i == 5 then",
+"if (i == 5)",
+"if i = 5 then"
+]
+},
+{
+question: "How can you add a comment in a JavaScript?",
+answer: "//This is a comment",
+options: [
+"!--This is a comment--",
+"//This is a comment",
+"'This is a comment",
+"None of the above"
+]
+},
+];
---
+++ myquiz-main/phpquest.js
@@ -0,0 +1,62 @@
+var phpquestions =[
+    {
+num : 1,
+question : "What does PHP stand for?",
+answer : "PHP: Hypertext Preprocessor",
+options : [
+"Private Home Page",
+"PHP: Hypertext Preprocessor",
+"Personal Hypertext Processor",
+"None of the above"
+        ]
+    },
+    {
+num: 2,
+question: "The PHP syntax is most similar to:",
+answer: "Perl and C",
+options: [
+"Perl and C",
+"JavaScript",
+"VBScript",
+"None of the above"
+]
+},
+{
+num: 3,
+question: "Which operator is used to check if two values are equal and of same data type?",
+answer: "===",
+options: [
+"!=",
+"==",
+"=",
+"==="
+]
+},
+
+{
+num: 4,
+question: "How do you create a cookie in PHP?",
+answer: "setcookie()",
+options: [
+"setcookie()",
+"createcookie",
+"makecookie()",
+"None of the above"
+]
+},
+{
+num: 5,
+question: "Which one of these variables has an illegal name?",
+answer: "$my-Var",
+options: [
+"$my_Var",
+"$myVar",
+"$my-Var",
+"All of the above"
+]
+},
+
+
+
+
+];
